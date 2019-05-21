6b6eb328534291a338f73a15547920181ca84bc1
Aaron Denney <aaron.m.denney@gmail.com>
2019-05-21 00:56:14 -0400

    added player attack sprites, fixed player sprite stuff

--- Player.h
+++ Player.h
@@ -3,25 +3,27 @@
 
 #include <Arduboy2.h>
 #include "bitmaps.h"
+
 class Player {
     Arduboy2 arduboy;
     Sprites sprites;
     enum Directions { Up, Down, Left, Right };
+    enum PlayerState { Idle, Walking, Attacking };
 
 private: 
+    unsigned char _numberOfAnimationsPerFacing = 4;
+    unsigned char _walkOffset = 1;
+    unsigned char _attackOffset = 3;
     unsigned char _spriteHeight = 16;
-    unsigned char _spriteWidth = 10;
+    unsigned char _spriteWidth = 24;
     unsigned char _healthMultiplier = 10;
     unsigned long _lastAttack = 0;
     unsigned long _attackSpeed = 1000;
+    unsigned long _attackDuration = 250;
+    unsigned char _playerState = Idle;
 
     Rect _playerCollider;
     Rect _weaponCollider;
-    // const unsigned char _idleSprite = CHAR_FORWARD_IDLE;
-    // const unsigned char _downWalkSprite = CHAR_FORWARD_WALKING;
-    // const unsigned char _leftWalkSprite = CHAR_LEFT_WALKING;
-    // const unsigned char _rightWalkSprite = CHAR_RIGHT_WALKING;
-    // const unsigned char _upWalkSprite = CHAR_BACK_WALKING;
 
 public:
     byte xPos;
@@ -33,14 +35,8 @@ public:
     Player() = default;
     Player(byte _level) {
         _spriteHeight = 16;
-        _spriteWidth = 10;
+        _spriteWidth = 12;
         _healthMultiplier = 10;
-
-        // _idleSprite = CHAR_FORWARD_IDLE;
-        // _downWalkSprite = CHAR_FORWARD_WALKING;
-        // _leftWalkSprite = CHAR_LEFT_WALKING;
-        // _rightWalkSprite = CHAR_RIGHT_WALKING;
-        // _upWalkSprite = CHAR_BACK_WALKING;
 
         level = _level;
         maxHealth = _level * _healthMultiplier;
@@ -62,44 +58,66 @@ public:
     
     void attack(){
         unsigned long currentMillis = millis();
+        
+        _playerState = Attacking;
+        
         if((currentMillis - _lastAttack) >= _attackSpeed ){
             byte width;
             byte height;
             byte x = xPos;
             byte y = yPos;
-            if(currentlyFacing == Up) { y = yPos - (24 - 8); width = 4; height = 24; };
-            if(currentlyFacing == Down) { y = yPos; width = 4; height = 24; };
-            if(currentlyFacing == Left) { x = xPos - (24 - 5); width = 24; height = 4; };
-            if(currentlyFacing == Right) { x = xPos; width = 24; height = 4; };
+            if(currentlyFacing == Up) { y = yPos - (8); width = 3; height = 8; };
+            if(currentlyFacing == Down) { y = yPos + (10); width = 3; height = 8; };
+            
+            if(currentlyFacing == Left) { y = yPos + (_spriteHeight / 2); x = xPos - (8); width = 8; height = 4; };
+            if(currentlyFacing == Right) { y = yPos + (_spriteHeight / 2); x = xPos + 10; width = 8; height = 4; };
             
             _weaponCollider = Rect(x, y, width, height);
-            arduboy.fillRect(x, y, width, height, BLACK);    
-            _lastAttack = millis();  
+            // arduboy.fillRect(x, y, width, height, BLACK);
+            // if(currentlyFacing == Up || currentlyFacing == Down)
+            //     arduboy.fillRect(x, y, width - 2, height, WHITE);
+            // if(currentlyFacing == Left || currentlyFacing == Right)
+            //     arduboy.fillRect(x, y, width, height - 2, WHITE);
+            drawAttack();
+            _lastAttack = millis();
+           
         }  
     };
+
     void shoot(){
 
     };
 
     void walkLeft(unsigned long counter) {
         currentlyFacing = Left;
-        sprites.drawPlusMask(xPos, yPos, CHAR_LEFT_WALKING, counter);
+        _playerState = Walking;
+        sprites.drawPlusMask(xPos, yPos, PLAYER_SPRITE, counter + (_numberOfAnimationsPerFacing * Left) + _walkOffset);
     };
     void walkRight(unsigned long counter) {
         currentlyFacing = Right;
-        sprites.drawPlusMask(xPos, yPos, CHAR_RIGHT_WALKING, counter);
+        _playerState = Walking;
+        sprites.drawPlusMask(xPos, yPos, PLAYER_SPRITE, counter + (_numberOfAnimationsPerFacing * Right) + _walkOffset);
     };
     void walkUp(unsigned long counter) {
         currentlyFacing = Up;
-        sprites.drawPlusMask(xPos, yPos, CHAR_BACK_WALKING, counter);
+        _playerState = Walking;    
+        sprites.drawPlusMask(xPos, yPos, PLAYER_SPRITE, counter + (_numberOfAnimationsPerFacing * Up) + _walkOffset);
     };
     void walkDown(unsigned long counter) {
         currentlyFacing = Down;
-        sprites.drawPlusMask(xPos, yPos, CHAR_FORWARD_WALKING, counter);
+        _playerState = Walking;
+        sprites.drawPlusMask(xPos, yPos, PLAYER_SPRITE, counter + (_numberOfAnimationsPerFacing * Down) + _walkOffset);
     };
-    void idle(unsigned long counter) {
-        sprites.drawPlusMask(xPos, yPos, CHAR_FORWARD_IDLE, counter);
+
+    void idle(unsigned long counter){
+        _playerState = Idle;
+        sprites.drawPlusMask(xPos, yPos, PLAYER_SPRITE, counter + (_numberOfAnimationsPerFacing * currentlyFacing));
     };
+
+    void drawAttack(){
+        sprites.drawPlusMask(xPos, yPos, PLAYER_SPRITE, (_numberOfAnimationsPerFacing * currentlyFacing) + _attackOffset);
+    };
+
     void removeWeaponCollider(){
         _weaponCollider = Rect();
     }
